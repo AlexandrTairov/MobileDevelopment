248c066ca44091e5eb79ee6f74b4b8af6d9c9938
Александр <sanek.tai0@gmail.com>
2021-02-15 17:30:59 +0400

    Worked menu and database entities

---
+++ app/src/main/java/com/example/mobile/database/entity/Product.java
@@ -0,0 +1,64 @@
+package com.example.mobile.database.entity;
+
+public class Product {
+
+    private int id;
+
+    private String productName;
+
+    private String description;
+
+    private String image;
+
+    private double discount;
+
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
